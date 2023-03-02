3d94e732a8783c6f80712ec0242ca0da00d11f44
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-03-02 14:13:49 -0600

    Moved testable code to lib/. Some cosmetic changes, like introduction of checkboxes instead of links and ability to deselect all cinemas in a single click.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,31 @@
 <script lang="ts">
-	import type { RootObject } from './+page'
-
-	const groupBy = (list: any[], key: string) =>
-		list.reduce((hash, obj) => ({ ...hash, [obj[key]]: (hash[obj[key]] || []).concat(obj) }), {})
-
-	const in_range = (x: number, from: number, to: number) => from <= x && x <= to
-
-	const float_to_hh_mm = (time: number) => {
-		return `${Math.floor(time)}:${(time % 1) * 60}`.replace(':0', ':00')
-	}
-
-	let [from, to] = [12, 23.5]
-
-	const to_float = (date: string) => {
-		const [hours, minutes] = new Date(date)
-			.toLocaleTimeString('en', { timeStyle: 'short', hour12: false })
-			.split(':')
-		return parseFloat(hours) + parseFloat(minutes) / 60
-	}
+	import { float_to_hh_mm, groupBy, in_range, to_float } from '../lib/util'
 
 	import type { PageData } from './$types'
 	export let data: PageData
 
+	const today = `Í dag, ${new Date()
+		.toLocaleDateString('is-IS', {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		})
+		.replace('dagur', 'daginn')}`
+
+	let [from, to] = [12, 23.5]
+
 	const all_cinemas = [
 		...new Set(
-			data.movies.flatMap((movie) => movie.showtimes.flatMap((showtime) => showtime.cinema))
+			data.movies?.flatMap((movie) => movie.showtimes.flatMap((showtime) => showtime.cinema))
 		)
 	]
-	let selected_cinemas = all_cinemas
 	$: cinemas_in_two_cols = [
 		all_cinemas.slice(0, Math.ceil(all_cinemas.length / 2)),
 		all_cinemas.slice(Math.ceil(all_cinemas.length / 2))
 	]
 
-	const toggle = (cinema: string) => {
-		selected_cinemas = selected_cinemas.includes(cinema)
-			? selected_cinemas.filter((c) => c !== cinema)
-			: [...selected_cinemas, cinema]
-	}
+	let selected_cinemas: string[] = all_cinemas
 
 	$: filtered_cinemas_showtimes = data.movies
 		.sort((a, b) => b.showtimes.length - a.showtimes.length)
@@ -62,51 +50,51 @@
 		}))
 </script>
 
-<header>
-	<div class="container">
-		<hgroup>
-			<h1>Hvað er í bíó?</h1>
-			<h2>
-				Í dag, {new Date()
-					.toLocaleDateString('is-IS', {
-						weekday: 'long',
-						year: 'numeric',
-						month: 'long',
-						day: 'numeric'
-					})
-					.replace('dagur', 'daginn')}
-			</h2>
-		</hgroup>
-	</div>
-</header>
 <main class="container">
+	<header>
+		<div class="container">
+			<hgroup>
+				<h1>Hvað er í bíó?</h1>
+				<h2>{today}</h2>
+			</hgroup>
+		</div>
+	</header>
 	<div class="grid">
 		<div style="white-space : break-spaces;">
-			<label>
-				Frá: {float_to_hh_mm(from)}
-				<input bind:value={from} type="range" min="12" max="23.5" step="0.5" />
-			</label>
-			<label>
-				Til: {float_to_hh_mm(to)}
-				<input bind:value={to} type="range" min="12" max="23.5" />
-			</label>
+			<!-- prettier-ignore -->
+			<div>
+				<label for="from">
+					Frá {float_to_hh_mm(from)}
+					<input bind:value={from} type="range" min="12" max="23.5" step="0.25" id="from" name="from"/>
+				</label>
+				<label for="to">
+					Til {float_to_hh_mm(to)}
+					<input bind:value={to} type="range" min="12" max="23.5" step="0.25" id="to" name="to"/>
+				</label>
+			</div>
 		</div>
 		<div>
 			<div class="grid">
-				{#each cinemas_in_two_cols as cinemas}
+				{#each cinemas_in_two_cols as cinemas, i}
 					<div>
 						<ul>
 							{#each cinemas as cinema}
-								<li>
-									<!-- prettier-ignore -->
-									<a style = {selected_cinemas.includes(cinema) ? undefined : "color: #606F79"}  href={'#'} on:click|preventDefault={() => toggle(cinema)}>{cinema}</a>
+								<li style="list-style-type: none;">
+									<small>
+										<!-- prettier-ignore -->
+										<input type="checkbox" bind:group={selected_cinemas} value={cinema} id={cinema} name={cinema} />
+										<label for={cinema}>{cinema}</label>
+									</small>
 								</li>
 							{/each}
 						</ul>
 					</div>
 				{/each}
 			</div>
-			<br />
+			<!-- prettier-ignore -->
+			<small>
+				<a href="/" style="text-align: center;" on:click={() => { selected_cinemas = selected_cinemas.length === 0 ? all_cinemas : [] }}>{selected_cinemas.length === 0 ? 'Velja öll' : 'Afvelja'} kvikmyndahús</a>
+			</small>
 		</div>
 	</div>
 	{#each filtered_cinemas_showtimes as { title, poster_url, trailer_url, release_year, genres, showtimes, description }}
@@ -131,7 +119,7 @@
 								{#each times as { time, purchase_url }, i}
 									<!-- prettier-ignore -->
 									<small style='font-variant-numeric: tabular-nums;'>
-											<a href={purchase_url}>{new Date(time).toLocaleTimeString('is-IS', { timeStyle: 'short', hour12: false })}&nbsp;</a>
+											<a href={purchase_url}>{new Date(time).toLocaleTimeString('is-IS', { timeStyle: 'short', hour12: false })}</a>&nbsp;&nbsp;
 									</small>
 								{/each}
 							</div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,39 +1,52 @@
 import type { PageLoad } from './$types'
+import { error } from '@sveltejs/kit'
+import { z } from 'zod'
 
-interface Showtime {
-	time: string
-	cinema: string
-	purchase_url: string
-	hall: string
-	tags: string[]
-}
+const kvikmyndir_schema = z.array(
+	z
+		.object({
+			title: z.string(),
+			alt_title: z.string(),
+			kvikmyndir_is_id: z.number(),
+			release_year: z.number(),
+			poster_url: z.string(),
+			content_rating_in_years: z.number(),
+			scrape_url: z.string(),
+			description: z.string(),
+			genres: z.array(z.string()),
+			duration_in_mins: z.number(),
+			rating_urls: z.array(z.string()),
+			language: z.string(),
+			showtimes: z.array(
+				z.object({
+					time: z.string(),
+					cinema: z.string(),
+					purchase_url: z.string(),
+					hall: z.string(),
+					tags: z.array(z.string())
+				})
+			),
+			trailer_url: z.string()
+		})
+		.strict()
+)
 
-export interface RootObject {
-	title: string
-	alt_title: string
-	kvikmyndir_is_id: number
-	release_year: number
-	poster_url: string
-	content_rating_in_years: number
-	scrape_url: string
-	description: string
-	genres: string[]
-	duration_in_mins: number
-	rating_urls: string[]
-	language: string
-	showtimes: Showtime[]
-	trailer_url: string
-}
+export type Kvikmyndir = z.infer<typeof kvikmyndir_schema>
 
 export const load = (async ({ fetch }) => {
 	const fetchMovies = async () => {
 		const response = await fetch('kvikmyndir_is.json')
-		return (await response.json()) as RootObject[]
+
+		const parsed = kvikmyndir_schema.safeParse(await response.json())
+		if (parsed.success) {
+			return { movies: parsed.data }
+		}
+
+		console.error(parsed.error)
+		throw error(500, 'Unable to parse JSON')
 	}
 
-	return {
-		movies: fetchMovies()
-	}
+	return fetchMovies()
 }) satisfies PageLoad
 
-// export const prerender = true
+export const prerender = true
