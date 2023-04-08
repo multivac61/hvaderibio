e7f2f566de546a405c941eb803d9cb0cfb5d43d0
Jökull Sólberg <jokull@solberg.is>
2023-04-08 18:32:26 +0000

    modal for movie showtimes

--- src/lib/Movie.svelte
+++ src/lib/Movie.svelte
@@ -2,58 +2,16 @@
 	import type { Movie } from '$lib/schemas'
 
 	export let movie: Pick<Movie, 'poster_url' | 'title' | 'description' | 'trailer_url'>
-	export let showtimes: [string, Movie['showtimes']][]
-
-	let selected = false
+	export let on_click: () => void
 </script>
 
-<div
-	class={`${
-		selected ? 'col-span-full' : ''
-	} flex flex-col sm:flex-row sm:items-start gap-4 md:gap-8 z-50`}
->
-	<button
-		on:click={() => {
-			selected = !selected
-		}}
-		class={selected ? 'shrink-0 sm:w-80' : ''}
-	>
+<div class="flex flex-col sm:flex-row sm:items-start gap-4 md:gap-8 z-50">
+	<button on:click={on_click}>
 		<img
 			src={movie.poster_url}
 			title={movie.title}
 			alt={movie.title}
-			class={`object-cover rounded-xl shadow-2xl aspect-[2/3] sm:w-[min(100%,360px)] ${
-				selected ? '' : ''
-			}`}
+			class="object-cover rounded-xl shadow-2xl aspect-[2/3] sm:w-[min(100%,360px)]"
 		/>
 	</button>
-	{#if selected}
-		<div class="grow">
-			<div class="font-bold mb-2 text-lg md:text-2xl">{movie.title}</div>
-			<div class="text-sm mb-4">
-				{movie.description} —
-				<a class="underline" href={movie.trailer_url} target="_blank"> Trailer. </a>
-			</div>
-			<div class="space-y-4">
-				{#each showtimes as [cinema, times] (cinema)}
-					<div>
-						<div class="mb-2 font-bold">{cinema}</div>
-						<div class="inline-flex flex-wrap gap-3">
-							{#each times as { time, purchase_url } (purchase_url)}
-								<a
-									class="tabular-nums px-2.5 py-1 rounded bg-slate-800 text-white shadow"
-									href={purchase_url}
-								>
-									{new Date(time).toLocaleTimeString('is-IS', {
-										timeStyle: 'short',
-										hour12: false
-									})}
-								</a>
-							{/each}
-						</div>
-					</div>
-				{/each}
-			</div>
-		</div>
-	{/if}
 </div>
---
+++ src/lib/Showtimes.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Movie } from '$lib/schemas'
+
+	export let showtimes: [string, Movie['showtimes']][]
+</script>
+
+<div>
+	<div class="space-y-4">
+		{#each showtimes as [cinema, times] (cinema)}
+			<div>
+				<div class="mb-2 font-bold">{cinema}</div>
+				<div class="inline-flex flex-wrap gap-3">
+					{#each times as { time, purchase_url } (purchase_url)}
+						<a
+							class="tabular-nums px-2.5 py-1 rounded-md bg-neutral-800 text-white"
+							href={purchase_url}
+						>
+							{new Date(time).toLocaleTimeString('is-IS', {
+								timeStyle: 'short',
+								hour12: false
+							})}
+						</a>
+					{/each}
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
