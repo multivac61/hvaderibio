5429193d28ff394bba5f8b7921f58b3319ba8b26
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-04-09 06:34:12 +0000

    Add scale on hover effect

--- src/lib/Movie.svelte
+++ src/lib/Movie.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
-	import type { Movie } from '$lib/schemas'
+	import type { Movie, Showtimes } from '$lib/schemas'
 
 	export let movie: Pick<Movie, 'poster_url' | 'title' | 'description' | 'trailer_url'>
-	export let on_click: () => void
+	export let showtimes: [string, Showtimes][]
+
+	let selected = false
 </script>
 
-<div class="flex flex-col sm:flex-row sm:items-start gap-4 md:gap-8 z-50">
-	<button on:click={on_click}>
+<div
+	class={`${
+		selected ? 'col-span-full' : ''
+	} flex flex-col sm:flex-row sm:items-start gap-4 md:gap-8`}
+>
+	<button
+		on:click={() => {
+			selected = !selected
+		}}
+		class={selected ? 'shrink-0 sm:w-80' : ''}
+	>
 		<img
 			src={movie.poster_url}
 			title={movie.title}
 			alt={movie.title}
-			class="object-cover rounded-xl shadow-2xl aspect-[2/3] sm:w-[min(100%,360px)]"
+			class={`object-cover rounded-lg shadow-2xl aspect-[2/3] sm:w-[min(100%,360px)] ${
+				selected ? '' : ''
+			} hover:scale-105 hover:z-50 transition-all`}
 		/>
 	</button>
 </div>
