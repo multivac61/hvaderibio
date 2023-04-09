349445bd5efe503f247bb457abd89747a9447967
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-04-09 06:28:04 +0000

    Rounded-md consistency

--- src/lib/CinemaTab.svelte
+++ src/lib/CinemaTab.svelte
@@ -8,5 +8,5 @@
 	on:click={on_click}
 	class={`${
 		is_selected ? 'bg-white bg-opacity-10 backdrop-blur-xl' : 'text-neutral-400'
-	} rounded-lg px-3 py-1.5 text-sm font-medium hover:text-white`}>{label}</button
+	} rounded-md px-3 py-1.5 text-sm font-medium hover:text-white`}>{label}</button
 >
