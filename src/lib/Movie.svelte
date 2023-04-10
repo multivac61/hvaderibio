335364cb0da27dbe88a98435d95697972c7d05c8
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-04-10 10:11:44 +0000

    Fill object and rounder corners

--- src/lib/Movie.svelte
+++ src/lib/Movie.svelte
@@ -10,6 +10,6 @@
 		src={movie.poster_url}
 		title={movie.title}
 		alt={movie.title}
-		class="object-contain aspect-[2/3] rounded-md shadow-2xl sm:w-[min(100%,360px)] sm:hover:scale-105 sm:hover:z-50 sm:transition-all"
+		class="object-fill aspect-[2/3] rounded-lg shadow-2xl sm:w-[min(100%,360px)] sm:hover:scale-105 sm:hover:z-50 sm:transition-all"
 	/>
 </button>
