1e7c103095b8aef358a15e00346f2fbca0668350
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-04-19 10:54:49 +0000

    Download images, resize and save as .webp

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,29 @@ import { movies_schema } from '$lib/schemas.js'
 import { error } from '@sveltejs/kit'
 import type { IMDbMovie } from '$lib/schemas'
 import ky from 'ky'
+import sharp from 'sharp'
+import fs from 'fs/promises'
+import path from 'path'
 
 export const prerender = true
+
+async function ensureDirectoryExists(dirPath: string) {
+	try {
+		await fs.access(dirPath)
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			await fs.mkdir(dirPath, { recursive: true })
+		} else {
+			throw error
+		}
+	}
+}
+
+async function main() {
+	// Your code to download and save the poster
+}
+
+main()
 
 export async function load({ fetch }) {
 	const fetchMovies = async () => {
@@ -18,10 +39,47 @@ export async function load({ fetch }) {
 		throw error(500, 'Unable to parse JSON')
 	}
 
+	async function downloadPoster(url: string) {
+		console.log('Downloading poster', url)
+
+		try {
+			const response = await ky.get(url)
+			const imageBuffer = await response.arrayBuffer()
+			const webpBuffer = await sharp(Buffer.from(imageBuffer))
+				.webp()
+				.resize({
+					width: 274,
+					height: 411,
+					fit: sharp.fit.cover,
+					position: sharp.strategy.attention
+				})
+				.toBuffer()
+			const fileName = path.join(
+				'static',
+				'posters',
+				`${path.basename(url, path.extname(url))}.webp`
+			)
+			await fs.writeFile(fileName, webpBuffer)
+			return path.join('posters', `${path.basename(url, path.extname(url))}.webp`)
+		} catch (e) {
+			console.error(e)
+		}
+	}
+
+	const postersDir = path.join('static', 'posters')
+	await ensureDirectoryExists(postersDir)
+
 	return {
 		movies: await fetchMovies().then(async (movies) => {
 			return Promise.all(
 				movies.map(async (movie) => {
+					try {
+						const poster = await downloadPoster(movie.poster_url)
+						movie.poster_url = poster as string
+					} catch (e) {
+						console.error(e)
+					}
+
 					const imdbLink = movie.rating_urls.find((link) => link.includes('imdb.com'))
 					if (imdbLink) {
 						const imdbId = new URL(imdbLink).pathname.split('/').at(-1)
