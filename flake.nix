2271c2a8005f0ea771102fc74e38c0800b745233
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-03 22:37:39 +0000

    Update dependencies and use nix as a package manager (pnpm is still installing node dependencies, building, etc)

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development shell for hvaderibio.is";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = { pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs.nodePackages_latest; [ pnpm nodejs ];
+        };
+      };
+    };
+}
--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,9 @@ import sharp from "sharp";
 
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
 import { preprocessMeltUI } from "@melt-ui/pp";
 import sequence from "svelte-sequential-preprocessor";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
