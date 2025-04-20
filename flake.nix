3eeea3f1cfb2b40d3fcf812f243093d55c887523
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-20 12:42:07 +0000

    feat(nix): get nix code up to date and add format checking

--- flake.nix
+++ flake.nix
@@ -1,34 +1,18 @@
 {
-  description = "Development shell for hvaderibio.is";
+  description = "Hvað er í bíó?";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      perSystem =
-        {
-          config,
-          self',
-          inputs',
-          pkgs,
-          lib,
-          system,
-          ...
-        }:
-        {
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
-          packages.nodejs = pkgs.nodejs_20;
-          packages.default = pkgs.callPackage ./default.nix { };
-        };
+    inputs:
+    inputs.blueprint {
+      inherit inputs;
+      prefix = "nix";
     };
 }
