0eff9959c53630ebbdb5ac87965124e96254e0c1
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-09-18 08:38:15 +0000

    chore(nix): move everything into a single flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,61 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    blueprint.url = "github:numtide/blueprint";
-    blueprint.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    inputs:
-    inputs.blueprint {
-      inherit inputs;
-      prefix = "nix";
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+      systems,
+    }:
+    let
+      eachSystem =
+        f: nixpkgs.lib.genAttrs (import systems) (system: f system nixpkgs.legacyPackages.${system});
+
+      systemOutputs = eachSystem (
+        _system: pkgs:
+        let
+          treefmtEval = treefmt-nix.lib.evalModule pkgs {
+            programs.deadnix.enable = true;
+            programs.nixfmt.enable = true;
+
+            programs.shellcheck.enable = true;
+            programs.shfmt.enable = true;
+
+            settings.formatter.shfmt.includes = [ "*.envrc" ];
+
+            settings.global.excludes = [
+              "*.png"
+              "*.jpg"
+              "*.zip"
+              "*.touchosc"
+              "*.pdf"
+              "*.svg"
+              "*.ico"
+              "*.webp"
+              "*.gif"
+            ];
+          };
+        in
+        {
+          formatter = treefmtEval.config.build.wrapper;
+          checks.formatting = treefmtEval.config.build.check self;
+          devshell.default = pkgs.mkShell {
+            packages = with pkgs; [
+              pnpm
+              nodejs_20
+            ];
+          };
+        }
+      );
+    in
+    {
+      devShells = nixpkgs.lib.mapAttrs (_system: outputs: outputs.devshell) systemOutputs;
+      formatter = nixpkgs.lib.mapAttrs (_system: outputs: outputs.formatter) systemOutputs;
+      checks = nixpkgs.lib.mapAttrs (_system: outputs: outputs.checks) systemOutputs;
     };
 }
