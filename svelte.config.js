d9bdef1ec20027e301733db12d59356b31b85945
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-09-22 14:26:09 +0000

    feat: add support for cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,27 @@
-import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+// Dynamically choose adapter based on environment variable
+const getAdapter = async () => {
+  if (process.env.ADAPTER === 'cloudflare') {
+    const adapterCloudflare = (await import("@sveltejs/adapter-cloudflare")).default;
+    return adapterCloudflare({
+      // Cloudflare Pages specific options
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    });
+  } else {
+    const adapterStatic = (await import("@sveltejs/adapter-static")).default;
+    return adapterStatic();
+  }
+};
 
 export default {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors for more information about preprocessors
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: await getAdapter(),
     prerender: { handleHttpError: "warn" },
     paths: { base: "" },
   },
