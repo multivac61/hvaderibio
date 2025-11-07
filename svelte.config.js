5e44eca5a476b8c0dabc9c7c42bd56cf2d939b09
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-07 16:03:47 +0000

    fix: cloudflare adaptor warnings
    > Using @sveltejs/adapter-cloudflare
      Cloudflare Pages Functions' includes/excludes exceeds _routes.json
    limits (see
    https://developers.cloudflare.com/pages/platform/functions/routing/#limits).
    Dropping 35 exclude rules — this will cause unnecessary function
    invocations.
      ✔ done

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,20 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const getAdapter = async () => {
   if (process.env.ADAPTER === "cloudflare") {
     const adapterCloudflare = (await import("@sveltejs/adapter-cloudflare")).default;
-    return adapterCloudflare();
+    return adapterCloudflare({
+      routes: {
+        include: ['/*'],
+        exclude: [
+          '/_app/*',
+          '/*.webp',
+          '/*.json',
+          '/*.txt',
+          '/*.ico',
+          '/*.xml',
+          '/movie/*'
+        ]
+      }
+    });
   } else {
     const adapterStatic = (await import("@sveltejs/adapter-static")).default;
     return adapterStatic();
