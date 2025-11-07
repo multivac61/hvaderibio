cf47bde52c82873658f1b001ab6db5bda0db8f92
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-07 14:35:16 +0000

    feat: move data loading from global layout.server to only index server page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,1 +1,8 @@
-export const prerender = true;
+import { movies_schema } from "$lib/schemas";
+import { readFile } from "fs/promises";
+
+export const load = async () => {
+  return {
+    movies: movies_schema.parse(JSON.parse(await readFile("static/movies.json", "utf-8"))),
+  };
+};
